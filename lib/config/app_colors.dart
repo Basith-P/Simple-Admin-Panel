bb3cba14ac753af23e2def1549f8d9bf28142b55
Basith-P <basithpofficial@gmail.com>
2022-05-19 18:50:43 +0530

    :lipstick: Change snackbar style

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -3,5 +3,6 @@ import 'dart:ui';
 class AppColors {
   static const Color primary = Color(0xFF252432);
   static const Color primaryLight = Color(0xFF383645);
+  static const Color primaryLighter = Color.fromARGB(255, 85, 90, 110);
   static const Color primaryDark = Color(0xFF1A1A24);
 }
--- lib/config/app_theme.dart
+++ lib/config/app_theme.dart
@@ -5,5 +5,13 @@ import 'app_colors.dart';
 class AppTheme {
   static ThemeData get darkTheme => ThemeData.dark().copyWith(
         scaffoldBackgroundColor: AppColors.primaryDark,
+        snackBarTheme: SnackBarThemeData(
+          backgroundColor: AppColors.primaryLighter,
+          behavior: SnackBarBehavior.floating,
+          contentTextStyle: const TextStyle(color: Colors.white),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
       );
 }
