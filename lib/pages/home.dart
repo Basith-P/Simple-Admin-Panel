f24e0a5a8efd5e4b531cd9c6bd8c9235175b2e32
Basith-P <basithpofficial@gmail.com>
2022-05-19 20:02:03 +0530

    :clown_face: Add users list with mock data

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../config/app_colors.dart';
+import '../widgets/user_card.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,6 +11,17 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Users'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: 15,
+          itemBuilder: (_, i) => UserCard(
+            name: 'User $i',
+            status: 'active',
+          ),
+        ),
       ),
     );
   }
